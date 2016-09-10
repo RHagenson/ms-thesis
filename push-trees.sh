34745f2d600359c8d8f096e67e48dc4ed53871eb
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2016-09-09 19:49:34 -0500

    Added simple Bash script to push all trees, must remain in root

---
+++ push-trees.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# enter subtree names with space between each entry
+subtrees=(python R LaTeX)
+
+echo "Number of subtrees: ${#subtrees[*]}"
+
+# Push each subtree with standard command
+echo "Pushing with 'git subtree push' to master branch using --squash"
+for item in ${subtrees[*]}
+do
+    git subtree push --prefix=$item --squash $item master
+    echo ""
+done
