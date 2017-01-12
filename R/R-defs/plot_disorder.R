c32e3bdd8b5ed79a5b1be10691d7073b0bf9baa8
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2017-01-11 22:52:09 -0600

    Uses ggplot2 now, but does not display entropy (still calculated though)

--- R/R-defs/plot_disorder.R
+++ R/R-defs/plot_disorder.R
@@ -5,6 +5,7 @@
 source("R-defs/extract_disorder.R")
 source("R-defs/corner_text.R")
 library(entropy)
+library(ggplot2)
 
 plot_disorder <- function(cancer_type, isoform_name) {
   # The assumed directory tree is based off running script in ms-thesis/R/
@@ -27,34 +28,47 @@ plot_disorder <- function(cancer_type, isoform_name) {
   # Extract the data.frame of Position, AA, Disorder
   iso_disorder <- extract_disorder(cancer_type = cancer_type, isoform_name = isoform_name)
   
-  # Create the 
+  # Create the path for PDF if it does not exist
   if(! dir.exists(pdfPath)){
-    dir.create(pdfPath, recursive = TRUE)  # Create path if it does not exist
+    dir.create(pdfPath, recursive = TRUE)
   }
   
   # Open the output pdf for writing, with naming based on where the profile is from
   pdfName = paste(isoform_name, ".pdf", sep="")
-  pdf(paste(pdfPath, pdfName, sep = "/"))
+  #pdf(paste(pdfPath, pdfName, sep = "/"))
   
-  # Generate plot
-  plot(x = iso_disorder$Position,
-       y = iso_disorder$Disorder,
-       xlab = "Position Number", 
-       ylab = "Disorder Score", 
-       type = "p", 
-       main = as.character(isoform_name), 
-       pch = 20
-  )
+  # plot(x = iso_disorder$Position,
+  #      y = iso_disorder$Disorder,
+  #      xlab = "Position Number", 
+  #      ylab = "Disorder Score", 
+  #      type = "p", 
+  #      main = as.character(isoform_name), 
+  #      pch = 20
+  # )
   
   # Calculate plot descriptors
   entropy <- round(entropy(iso_disorder$Disorder, frequency(iso_disorder$Disorder)), digits = 4)
   avg_disorder <- round(ave(iso_disorder$Disorder), digits = 4)
   
   # Add description to plot
-  corner_text(text = c(paste("Entropy: ", entropy, "\n",
-                           "Average disorder: ", avg_disorder, sep = "")))
+  #corner_text(c(paste("Entropy: ", entropy, "\n",
+  #                  "Average disorder: ", avg_disorder, "\n", sep = "")))
+  
+  # Generate plot
+  ggplot(iso_disorder, aes(x = Position,
+                           y = Disorder)) +
+    geom_point() +
+    ggtitle(isoform_name) +
+    xlab("Position Number") +
+    ylab("Disorder Score")
+    # geom_label(vjust="", 
+    #            label = paste("Entropy: ", entropy, "\n",
+    #                          "Average disorder: ", avg_disorder, 
+    #                          sep = ""))
+    # 
+  # Save file
+  ggsave(paste(pdfPath, pdfName, sep = "/"), device = "pdf")
   
   # Close the graphic device to save to file
-  dev.off()
-  
+  #dev.off()
 }
