308a4eff729d841d7cc09a49d16bc11de5c5c7c2
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2017-01-25 23:39:35 -0600

    Finalized Rscript that processes results for collaborator

--- R/ProcessResults_ForCollab.R
+++ R/ProcessResults_ForCollab.R
@@ -8,6 +8,7 @@
 # Email: rhagenson@unomaha.edu
 
 library(optparse)
+source("R-defs/extract_mutations.R")
 
 # Define the CLI arguments
 option_list = list(
@@ -33,15 +34,76 @@ disorder_table <- read.delim("processing/disorderPositions/DisorderPositionResul
 # disorder_table <- read.delim(opt$disorder, stringsAsFactors = FALSE)
 
 # Define internal variables
-new_frame <- data.frame(Gene=unique(disorder_table$Gene), Types=length(unique(disorder_table$Gene)))
+new_frame <- data.frame(Gene=unique(disorder_table$Gene), 
+                        Types=NA,
+                        Isoforms=NA,
+                        Mutations=NA)
 
+# Build new_frame$Types and new_frame$Isoform entries with uniqueness
 for (gene in new_frame$Gene) {
   # Find what indices that gene is found in original table
   gene_indices <- which(disorder_table$Gene == gene)
   
-  # Get cancer types
-  cancers <- disorder_table[gene_indices, "Type"]
   
-  # Set new_frame at index to cancers
-  new_frame$Types[which(new_frame$Gene == gene)] = paste(unique(cancers), collapse=',')
+  # Gather cancer types and isoform numbers into data.frame
+  temp_frame <- data.frame(Types=disorder_table[gene_indices, "Type"],
+                           Numbers=disorder_table[gene_indices, "Isoform"])
+  
+  # Remove duplicate rows
+  temp_frame <- temp_frame[!duplicated(temp_frame), ]
+  
+  # Set new_frame to unique values
+  new_frame$Types[which(new_frame$Gene == gene)] = paste0(temp_frame$Types, collapse = ",")
+  new_frame$Isoforms[which(new_frame$Gene == gene)] = paste0(temp_frame$Numbers, collapse = ",")
+}
+
+# Loop through rows of new_frame and output formatted results
+for (index in 1:length(new_frame$Gene)) {
+  gene <- new_frame[index, "Gene"]
+  
+  # Build cancer type and isoform number vectors
+  cancer_vector <- as.list(strsplit(new_frame[index, "Types"], ",")[[1]])
+  iso_num_vector <- as.list(strsplit(new_frame[index, "Isoforms"], ",")[[1]])
+  
+  # Set/Reset mutations_list to NULL
+  mutations_list <- c()
+  
+  # Loop through indexes to retrieve mutations in 'A251G' format
+  for (sec_index in 1:length(cancer_vector)) {
+    mutations_list[sec_index] <- paste(extract_mutations(cancer_type = cancer_vector[sec_index], 
+                                                         gene = gene,
+                                                         isoform_num = iso_num_vector[sec_index]),
+                                   collapse = ",")
+    
+  }
+  
+  # Collapse the multiple mutations_list entries into a single string
+  new_frame[index, "Mutations"] <- paste(mutations_list, collapse = ";")
+  
+  output_lines <- vector(length = length(new_frame$Gene))
+  
+  # Prepare to output to file
+  for (index in 1:length(new_frame$Gene)) {
+    # Start output line with Gene
+    next_line <- new_frame[index, "Gene"]
+    
+    # Extract Types and Mutations lists
+    Types <- as.list(strsplit(new_frame[index, "Types"], split = ",")[[1]])
+    Muts <- as.list(strsplit(new_frame[index, "Mutations"], split = ";")[[1]])
+    
+    # Next loop in parallel over Types and Mutations continually adding to output
+    for (sec_index in 1:length(Types)) {
+      # Add next type
+      next_line <- paste(next_line, Types[sec_index], sep="\t")
+      
+      # Add next set of mutations
+      next_line <- paste(next_line, Muts[sec_index], sep=" ")
+    }
+    
+    # Add line to output vector
+    output_lines[index] <- next_line
+  }
+  
+  # Output to file
+  write(output_lines, "/tmp/test_out_boih")
 }
